ff05dbff5f949712cded81d71167f6f035d9d971
sahithipriya07 <lpriya1@umbc.edu>
2025-02-13 20:28:16 -0500

    Add files via upload

---
+++ create_ec2.py
@@ -0,0 +1,23 @@
+import boto3
+
+# Initialize a session using Boto3
+ec2 = boto3.resource('ec2', region_name='us-east-1')
+
+# Create an EC2 instance
+instance = ec2.create_instances(
+    ImageId='ami-0c02fb55956c7d316',  # Amazon Linux 2 AMI (Free Tier Eligible)
+    MinCount=1,
+    MaxCount=1,
+    InstanceType='t2.micro',
+    KeyName='MyKeyPair',  # Ensure you have created this key pair
+    TagSpecifications=[
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {'Key': 'Name', 'Value': 'MyPythonEC2Instance'}
+            ]
+        }
+    ]
+)
+
+print(f'Created instance with ID: {instance[0].id}')
