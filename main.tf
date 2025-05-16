23f4abac9a199f458407b3b2fbe7bfaf973551db
Sahithi <lpriya1@umbc.edu>
2025-05-15 20:32:08 -0400

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,19 +1,16 @@
-variable "instances" {
-  type = map
-  default = {
-    "web1" = "t2.micro"
-    "web2" = "t3.micro"
-    "web3" = "t2.small"
-  }
+resource "aws_vpc" "my_vpc_project" {
+  cidr_block = "10.0.0.0/16"  # Example: 10.0.0.0/16
 }
 
-resource "aws_instance" "web" {
-  for_each = var.instances
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.my_vpc_project.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone        = "us-east-1a"
+}
 
-  ami           = "ami-071226ecf16aa7d96"
-  instance_type = each.value
-
-  tags = {
-    Name = "${each.key}"
-  }
+resource "aws_subnet" "private_subnet" {
+  vpc_id           = aws_vpc.my_vpc_project.id
+  cidr_block       = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
 }
